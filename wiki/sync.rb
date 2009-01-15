66994c1f631c58ae670dd23abe03574240ff68f2
Steve Yen <steve.yen@gmail.com>
2009-01-15 10:27:21 -0800

    updated README with more instructions, and sync.rb takes an optional checkin msg 1st argument

--- wiki/sync.rb
+++ wiki/sync.rb
@@ -1,9 +1,13 @@
 #!/usr/bin/ruby
 
 now = Time.now.strftime('%Y/%m/%d %H:%M:%S')
-me = `whoami`.strip
+usr = `whoami`.strip
+msg = ARGV[0]
+msg = "- #{msg}" if msg
 
+# Dumb, always "works" merge strategy of local changes win.
+#
 `git pull --strategy ours origin master`
 `git add .`
-`git commit -m "syncing by #{me} on #{now}"`
+`git commit -m "syncing by #{usr} on #{now} #{msg}"`
 `git push origin master`
