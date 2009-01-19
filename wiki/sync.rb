a7e5ada19636400fc1ec3522a8c8401299b12dcb
Steve Yen <steve.yen@gmail.com>
2009-01-18 22:16:45 -0800

    sync.rb checkin msg as first argument is now mandatory

--- wiki/sync.rb
+++ wiki/sync.rb
@@ -1,13 +1,8 @@
 #!/usr/bin/ruby
 
-now = Time.now.strftime('%Y/%m/%d %H:%M:%S')
-usr = `whoami`.strip
-msg = ARGV[0]
-msg = "- #{msg}" if msg
-
 # Dumb, always "works" merge strategy of local changes win.
 #
 `git pull --strategy ours origin master`
 `git add .`
-`git commit -m "syncing by #{usr} on #{now} #{msg}"`
+`git commit -m "#{ARGV[0]}"`
 `git push origin master`
